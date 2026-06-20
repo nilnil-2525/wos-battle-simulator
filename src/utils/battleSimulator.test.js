1826fb5f1de6b54d5ef79726d9b101e029e6e6d6
nilnil-2525 <montakky.friedchicken@gmail.com>
2026-06-20 14:00:55 +0900

    test: add test cases for Jeronimo, Edith, Gordon, Bradley, and Hendrick skills

--- src/utils/battleSimulator.test.js
+++ src/utils/battleSimulator.test.js
@@ -83,10 +83,12 @@ describe('WOS Battle Simulator Table-Driven Tests', () => {
 
             // 3. 進行処理の実行
             // 特殊な発動ターンのある固有スキルに対応するため実行ターン数を決定
-            let runTurn = 1;
-            if (tc.ally?.sonya_active) runTurn = 6;
-            else if (tc.ally?.hendrick_active) runTurn = 3;
-            else if (tc.ally?.mia_i_active) runTurn = 2;
+            let runTurn = tc.runTurn || 1;
+            if (!tc.runTurn) {
+                if (tc.ally?.sonya_active) runTurn = 6;
+                else if (tc.ally?.hendrick_active) runTurn = 3;
+                else if (tc.ally?.mia_i_active) runTurn = 2;
+            }
             
             let battleState = currentArmyData;
             let fixedMinTroops = Math.min(
